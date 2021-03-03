b502f8486ca8dabff7725d3074e2fb5ebf3fb8db
jasonemarkson <jasonmarkson76@gmail.com>
2021-03-02 23:08:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
-
-}
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json)
+)}
 
 function renderBooks(books) {
   const main = document.querySelector('main')
@@ -8,7 +10,7 @@ function renderBooks(books) {
     const h2 = document.createElement('h2')
     h2.innerHTML = book.name
     main.appendChild(h2)
-  })
+  }) 
 }
 
 document.addEventListener('DOMContentLoaded', function() {
